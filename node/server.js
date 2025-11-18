944a0f6b6e7efc82740236fcecc828f19294348d
keerthi <keerthichinnasamy22@gmailcom>
2025-11-18 18:40:57 +0530

    adding

---
+++ node/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect DB
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
